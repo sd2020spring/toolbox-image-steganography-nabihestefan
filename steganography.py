6d9599958ce25813b6c98978f745c022d594ae28
nabihestefan <estefan.nabih@gmail.com>
2020-03-28 21:35:39 -0400

    Toolbox completed

--- steganography.py
+++ steganography.py
@@ -1,9 +1,9 @@
 """A program that encodes and decodes hidden messages in images through LSB steganography"""
 from PIL import Image, ImageFont, ImageDraw
 import textwrap
+import numpy as np
 
-
-def decode_image(file_location="images/encoded_sample.png"):
+def decode_image(file_location="images/encoded_sample.png", final_location="images/decoded_image.png"):
     """Decodes the hidden message in an image.
 
     Parameters
@@ -13,26 +13,24 @@ def decode_image(file_location="images/encoded_sample.png"):
         encoded image in the images folder.
     """
     encoded_image = Image.open(file_location)
-    red_channel = encoded_image.split()[0]
-    # The above could also be written as one of:
-    #   red_channel, green_channel, blue_channel = encoded_image.split()
-    #   red_channel, _, _ = encoded_image.split()
-    #   red_channel, *_ = encoded_image.split()
-    # The first has the disadvantage of creating temporary variables that aren't
-    # used. The special variable name _ (underscore) is conventionally named
-    # an unused variable.
 
     x_size = encoded_image.size[0]
     y_size = encoded_image.size[1]
-    # The above could also be written as:
-    #   x_size, y_size = encoded_image.size[0]
 
     decoded_image = Image.new("RGB", encoded_image.size)
-    pixels = decoded_image.load()
 
-    pass  # TODO: Fill in decoding functionality
+    for x in range(x_size):
+        for y in range(y_size):
+            pixel = encoded_image.getpixel((x,y))[0]
+            binary = bin(pixel)
+            if binary[-1:] == "0":
+                decoded_image.putpixel((x,y), (0,0,0))
+            elif binary[-1:] == "1":
+                decoded_image.putpixel((x,y), (255,255,255))
 
-    decoded_image.save("images/decoded_image.png")
+
+
+    decoded_image.save(final_location)
 
 
 def write_text(text_to_write, image_size):
@@ -54,6 +52,7 @@ def write_text(text_to_write, image_size):
     for line in textwrap.wrap(text_to_write, width=60):
         drawer.text((margin, offset), line, font=font)
         offset += 10
+    image_text.save("images/image_text.png")
     return image_text
 
 
@@ -67,7 +66,37 @@ def encode_image(text_to_encode, template_image="images/samoyed.jpg"):
     template_image: str
         The image to use for encoding. An image is provided by default.
     """
-    pass  # TODO: Fill out this function
+    template_image = Image.open(template_image)
+    x_size = template_image.size[0]
+    y_size = template_image.size[1]
+
+    write_text(text_to_encode, (x_size, y_size))
+    text_image = Image.open("images/image_text.png")
+
+    encoded_image = Image.new("RGB", template_image.size)
+
+    for x in range(x_size):
+        for y in range(y_size):
+            temp_pixel = template_image.getpixel((x,y))
+            text_pixel = text_image.getpixel((x,y))
+
+            red = temp_pixel[0]
+            green = temp_pixel[1]
+            blue  = temp_pixel[2]
+            og_binary = bin(red)
+
+            if text_pixel == (255,255,255):
+                new_binary = og_binary[:-1]
+                new_binary += "1"
+            elif text_pixel == (0,0,0):
+                new_binary = og_binary[:-1]
+                new_binary += "0"
+            new_red = int(new_binary, 2)
+            new_pixel = (new_red, green, blue)
+            encoded_image.putpixel((x, y), new_pixel)
+
+
+    encoded_image.save("images/myencoding.png")
 
 
 if __name__ == '__main__':
@@ -75,4 +104,29 @@ if __name__ == '__main__':
     decode_image()
 
     print("Encoding the image...")
-    encode_image()
+    encode_image("The story so far: In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move." +
+                 "                                                                                             " +
+                 "                                                                                             " +
+                 "'The Answer to the Ultimate Question of Life, The Universe, and Everything is...Forty-two,' said Deep Thought, with infinite majesty and calm."+
+                 "                                                                                             " +
+                 "                                                                                             " +
+                 " It is a well known fact that those people who most want to rule people are, ipso facto, those least suited to do it. To summarize the summary: anyone who is capable of getting themselves made President should on no account be allowed to do the job."+
+                 "                                                                                             " +
+                 "                                                                                             " +
+                 "There is an art, it says, or rather, a knack to flying. The knack lies in learning how to throw yourself at the ground and miss." +
+                 "                                                                                             " +
+                 "                                                                                             " +
+                 "Nothing travels faster than the speed of light with the possible exception of bad news, which obeys its own special laws." +
+                 "                                                                                             " +
+                 "                                                                                             " +
+                 "In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move." +
+                 "                                                                                             " +
+                 "                                                                                             " +
+                 "Don't Panic" +
+                 "                                                                                             " +
+                 "                                                                                             " +
+                 "                                                                                             " +
+                 "- The Ultimate Hitchhiker's Guide to the Galaxy by Douglas Adams")
+
+    print("Decoding your own image!")
+    decode_image("images/myencoding.png", "images/mydecoding.png")
